e7cafcb398d7d3abd07f368e91bfdd0e1449bd23
Hector B <hectorbg303@gmail.com>
2025-06-02 20:05:02 +0200

    Javascript

---
+++ index.js
@@ -0,0 +1,56 @@
+const form = document.querySelector(".form");
+const clearBtn = document.querySelector(".calculator__clear-btn");
+const results = document.querySelector(".results");
+const mortgageMonth = document.querySelector(".results__monthly span");
+const mortgageTotal = document.querySelector(".results__total span");
+
+function clearAll() {
+  form.reset();
+  results.classList.remove("submitted");
+}
+
+function submitForm(ev) {
+  ev.preventDefault();
+
+  const dataArr = [...new FormData(this)];
+  const data = Object.fromEntries(dataArr);
+  calculateMortgage(data);
+  results.classList.add("submitted");
+}
+
+function formatNumberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function typeRepayment(amount, months, monthInterest) {
+  return (
+    (Number(amount) * monthInterest * (1 + monthInterest) ** months) /
+    ((1 + monthInterest) ** months - 1)
+  );
+}
+
+function typeInterestOnly(amount, monthInterest) {
+  return Number(amount) * monthInterest;
+}
+
+function calculateMortgage({ amount, term, interest, type }) {
+  const monthInterest = Number(interest) / 1200;
+  const months = Number(term) * 12;
+
+  let monthly;
+  if (type === "repayment") {
+    monthly = typeRepayment(amount, months, monthInterest);
+  } else {
+    monthly = typeInterestOnly(amount, monthInterest);
+  }
+
+  mortgageMonth.innerHTML = formatNumberWithCommas(monthly.toFixed(2));
+  mortgageTotal.innerHTML = formatNumberWithCommas(
+    (monthly * months).toFixed(2)
+  );
+}
+
+(function init() {
+  clearBtn.addEventListener("click", clearAll);
+  form.addEventListener("submit", submitForm);
+})();
